0b057c15cadf8299282dea6c2ce1eedae8d482ce
Dede <specialized8393@gmail.com>
2025-05-20 14:43:51 +0100

    feat: create accounts app with base structure

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.accounts',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# for Django debug toolbar
+import mimetypes
+mimetypes.add_type("application/javascript", ".js", True)
