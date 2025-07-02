ae1802bc1f0dbbd01a1191c77a596ff61d3da591
Dede <specialized8393@gmail.com>
2025-07-02 18:27:04 +0100

    add AuthorAdmin to admin panel by Django

--- backend/apps/accounts/admin.py
+++ backend/apps/accounts/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from django.utils.translation import gettext as _
 
-from .models import User
+from .models import User, Author
 
 
 @admin.register(User)
@@ -38,3 +38,45 @@ class UserAdmin(DjangoUserAdmin):
     list_display = ("email", "first_name", "last_name", "is_staff")
     search_fields = ("email", "first_name", "last_name")
     ordering = ("email",)
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = (
+        "user_email",
+        "is_verified",
+        "profession",
+        "experience_years",
+        "rating",
+        "review_count",
+        "created_at",
+    )
+    list_filter = ("is_verified", "experience_years", "rating", "created_at")
+    search_fields = (
+        "user__email",
+        "profession",
+        "bio",
+        "languages",
+        "speciality",
+    )
+    autocomplete_fields = ("user",)
+    readonly_fields = ("created_at", "updated_at")
+
+    fieldsets = (
+        (None, {
+            "fields": ("user", "is_verified", "profession", "bio")
+        }),
+        (_("Experience & Ratings"), {
+            "fields": ("experience_years", "rating", "review_count")
+        }),
+        (_("Meta Info"), {
+            "fields": ("languages", "speciality", "social_media")
+        }),
+        (_("Timestamps"), {
+            "fields": ("created_at", "updated_at")
+        }),
+    )
+
+    def user_email(self, obj):
+        return obj.user.email
+    user_email.short_description = "User Email"
