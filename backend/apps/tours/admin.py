1496650297c0ed1ddba782a4adaeae1a89723811
Dede <specialized8393@gmail.com>
2025-07-02 18:27:50 +0100

    Implemented TourAdmin to Django admin panel

--- backend/apps/tours/admin.py
+++ backend/apps/tours/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.tours.models import Country, Destination, Tour, Date
+
+
+class DestinationInline(admin.TabularInline):
+    model = Destination
+    extra = 1
+
+
+class DateInline(admin.TabularInline):
+    model = Date
+    extra = 1
+
+
+@admin.register(Tour)
+class TourAdmin(admin.ModelAdmin):
+    inlines = (DateInline, DestinationInline, )
